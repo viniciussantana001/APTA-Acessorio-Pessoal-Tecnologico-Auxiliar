7c0b08c503f99717637bcc42654b248f4e437eb6
viniciussantana001 <sviniciussantos001@gmail.com>
2025-08-06 17:23:32 -0300

    Criação do projeto

---
+++ App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>O começo da tecnologia que cuida de você</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
--- README.md
+++
@@ -1,1 +0,0 @@
-# APTA-Acessorio-Pessoal-Tecnologico-Auxiliar
